36cdb2ac760160865d8e677cdbbb77700625289c
Leandro <leandrocabralf@gmail.com>
2021-10-07 21:36:03 -0300

    Added structure network

---
+++ data/src/main/java/br/com/leandroid/data/network/RetrofitFactory.kt
@@ -0,0 +1,23 @@
+package br.com.leandroid.data.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.Converter
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+
+internal class RetrofitFactory {
+    fun getRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory,
+        rxJava2CallAdapterFactory: RxJava2CallAdapterFactory
+    ): Retrofit {
+        val url = "https://s3.amazonaws.com/orama-media/json/"
+
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .addCallAdapterFactory(rxJava2CallAdapterFactory)
+            .build()
+    }
+}
