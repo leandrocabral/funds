1496c397ffdfe20497b095007386d4ad5b9ed71d
Leandro <leandrocabralf@gmail.com>
2021-10-07 22:07:00 -0300

    Added structure funds list and detail

---
+++ app/src/main/java/br/com/leandroid/funds/presentation/detailFunds/DetailFundsViewModel.kt
@@ -0,0 +1,4 @@
+package br.com.leandroid.funds.presentation.detailFunds
+
+class DetailFundsViewModel {
+}
---
+++ app/src/main/java/br/com/leandroid/funds/presentation/funds/FundsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.leandroid.funds.presentation.funds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.leandroid.funds.R
+
+class FundsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.funds_activity)
+    }
+}
