841a292ee1a0ac16a00c2ce79dc65929646b3219
Leandro <leandrocabralf@gmail.com>
2021-10-09 10:13:32 -0300

    Created useCase Funds

--- domain/src/main/java/br/com/leandroid/domain/usecase/FundsUseCase.kt
+++ domain/src/main/java/br/com/leandroid/domain/usecase/FundsUseCase.kt
@@ -1,4 +1,12 @@
 package br.com.leandroid.domain.usecase
 
-class FundsUseCase {
+import br.com.leandroid.data.network.funds.repository.FundsRepository
+import br.com.leandroid.domain.model.FundsDomain
+
+class FundsUseCase(private val remoteRepository: FundsRepository) {
+
+    suspend fun getFunds(): List<FundsDomain> {
+        var teset = remoteRepository.getFunds()
+        return arrayListOf(FundsDomain(fullName = teset!![0].fullName))
+    }
 }
