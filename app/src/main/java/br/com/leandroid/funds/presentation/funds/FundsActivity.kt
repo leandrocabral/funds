715f8cf236790b1fa4ae8a30e245a9c8040dfba2
Leandro <leandrocabralf@gmail.com>
2021-10-09 10:12:47 -0300

    Added activity and viewModel Funds

--- app/src/main/java/br/com/leandroid/funds/presentation/funds/FundsActivity.kt
+++ app/src/main/java/br/com/leandroid/funds/presentation/funds/FundsActivity.kt
@@ -3,10 +3,29 @@ package br.com.leandroid.funds.presentation.funds
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.leandroid.funds.R
+import br.com.leandroid.funds.presentation.util.observeResource
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FundsActivity : AppCompatActivity() {
+
+    private val viewModel by viewModel<FundsViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.funds_activity)
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        viewModel.getFunds()
+
+        viewModel.fundsList.observeResource(this,
+            onSuccess = {
+
+            }, onError = {
+
+            }, onLoading = {
+
+            })
     }
 }
--- app/src/main/java/br/com/leandroid/funds/presentation/funds/FundsViewModel.kt
+++ app/src/main/java/br/com/leandroid/funds/presentation/funds/FundsViewModel.kt
@@ -1,4 +1,29 @@
 package br.com.leandroid.funds.presentation.funds
 
-class FundsViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import br.com.leandroid.domain.model.FundsDomain
+import br.com.leandroid.domain.usecase.FundsUseCase
+import br.com.leandroid.funds.presentation.util.BaseViewModel
+import br.com.leandroid.funds.presentation.util.Resource
+import kotlinx.coroutines.launch
+
+class FundsViewModel(private val useCase: FundsUseCase) : BaseViewModel() {
+
+    val fundsList = MutableLiveData<Resource<List<FundsDomain>>>()
+
+    fun getFunds() {
+        viewModelScope.launch {
+            with(fundsList) {
+                loading(true)
+                try {
+                    success(useCase.getFunds())
+                } catch (e: Exception) {
+                    error(e)
+                } finally {
+                    loading(false)
+                }
+            }
+        }
+    }
 }
