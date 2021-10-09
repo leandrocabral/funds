81bbf752f2390caaf4cfedfcddf95181d6831272
Leandro <leandrocabralf@gmail.com>
2021-10-09 20:21:05 -0300

    Adjusta toolbar funds

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -13,7 +13,3 @@ h
 8app/src/main/java/br/com/leandroid/funds/MainActivity.kt,2\9\29283fd1b55e34bf87b244bd19bb7c11030c46a6
 ˆ
 Xapp/src/main/java/br/com/leandroid/funds/presentation/detailFunds/DetailFundsActivity.kt,c\c\ccb0257115a0e62f5c100ea5b40952cdf1111189
-v
-Fapp/src/main/java/br/com/leandroid/funds/presentation/util/Resource.kt,f\7\f7e0e3966a2d8bed1bfa4690ef43280067a4ae5d
-{
-Kapp/src/main/java/br/com/leandroid/funds/presentation/util/BaseViewModel.kt,c\e\cefe58b78045d8e75ec107f4e8be6e8ff2a0e2bf
--- ui/src/main/java/br/com/leandroid/ui/widget/ToolBarHeader.kt
+++ ui/src/main/java/br/com/leandroid/ui/widget/ToolBarHeader.kt
@@ -2,8 +2,12 @@ package br.com.leandroid.ui.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
 import br.com.leandroid.ui.R
 
 class ToolbarHeaderView
@@ -14,20 +18,52 @@ class ToolbarHeaderView
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private val title: TextView?
+    private val arrowBack: ImageView?
+    val search: ImageView?
+    val editSearch: EditText?
 
     init {
         inflate(context, R.layout.toolbar_header_widget, this)
         title = findViewById(R.id.title_toolbar_header)
+        arrowBack = findViewById(R.id.arrow_toolbar_header)
+        search = findViewById(R.id.search_toolbar_header)
+        editSearch = findViewById(R.id.edit_search_toolbar_header)
         setAttribute(attrs)
+        setupView()
     }
 
-    fun setType(type: String?) {
+    private fun setupView() {
+        search?.setOnClickListener {
+            if (editSearch?.isVisible == true) {
+                editSearch.visibility = View.GONE
+                title?.visibility = View.VISIBLE
+            } else {
+                editSearch?.visibility = View.VISIBLE
+                title?.visibility = View.GONE
+            }
+        }
+    }
 
+    private fun setType(type: String?) {
+        when (type) {
+            TYPE_SEARCH -> {
+                search?.visibility = View.VISIBLE
+            }
+            else -> {
+                arrowBack?.visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun setAttribute(attrs: AttributeSet?) {
         val attributeSet = context.obtainStyledAttributes(attrs, R.styleable.ToolbarHeaderWidget)
+        val type = attributeSet.getString(R.styleable.ToolbarHeaderWidget_type)
         title?.text = attributeSet.getString(R.styleable.ToolbarHeaderWidget_title)
+        setType(type)
         attributeSet.recycle()
+    }
+
+    companion object {
+        const val TYPE_SEARCH = "1"
     }
 }
