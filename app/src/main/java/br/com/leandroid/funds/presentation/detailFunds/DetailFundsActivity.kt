6ac46584fac0e0d22455dd9d9eb4de9a09e97c40
Leandro <leandrocabralf@gmail.com>
2021-10-10 15:09:15 -0300

    Started screen Detail

--- app/src/main/java/br/com/leandroid/funds/presentation/detailFunds/DetailFundsActivity.kt
+++ app/src/main/java/br/com/leandroid/funds/presentation/detailFunds/DetailFundsActivity.kt
@@ -1,12 +1,31 @@
 package br.com.leandroid.funds.presentation.detailFunds
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.leandroid.core.BindingActivity
+import br.com.leandroid.domain.model.FundsDomain
 import br.com.leandroid.funds.R
+import br.com.leandroid.funds.databinding.DetailFundsActivityBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class DetailFundsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.detail_funds_activity)
+class DetailFundsActivity : BindingActivity<DetailFundsActivityBinding>() {
+
+    private var fundsDomain: FundsDomain? = null
+
+    private val viewModel by viewModel<DetailFundsViewModel>()
+
+    override fun getLayoutId(): Int = R.layout.detail_funds_activity
+
+    override fun onCreate(saveInstanceState: Bundle?) {
+        super.onCreate(saveInstanceState)
+        fundsDomain = intent.getParcelableExtra(DETAIL_FUNDS_KEY)
+
+    }
+
+    private fun setupObserver() {
+
+    }
+
+    companion object {
+        const val DETAIL_FUNDS_KEY = "DETAIL_FUNDS"
     }
 }
